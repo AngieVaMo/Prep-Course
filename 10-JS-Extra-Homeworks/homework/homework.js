fc9c133d535f69ef61b7e534de6f21cad3204247
AngieVaMo <anvamopa2@gmail.com>
2022-02-10 19:07:48 -0500

    CAMBIO DE COMPUTADOR

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var NArray=[];
+  for(var clave in objeto){
+    NArray.push([clave, objeto[clave]])
+  }
+  return NArray;
 }
 
 
@@ -18,6 +23,14 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var objeto={};
+  for(var i=0; i<string.length; i++){
+    if(objeto.hasOwnProperty(string[i])){
+      objeto[string[i]] += 1;
+    } else{
+      objeto[string[i]]= 1;
+    }
+  }
 }
 
 
@@ -26,6 +39,11 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var MAYUS= "";
+  var minus= "";
+  for(var i=0; i<s.length; i++){
+    if(s[i]===s[i].toLowerCase)
+  }
 }
 
 
